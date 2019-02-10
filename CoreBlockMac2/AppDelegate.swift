91e456b8c3a001fd03a1b527467d6c69c82c989d
yhzhou <yhzhou.tom@gmail.com>
2019-02-10 16:19:02 +0800

    Initial Commit

---
+++ CoreBlockMac2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CoreBlockMac2
+//
+//  Created by Yihua Zhou on 2019/2/10.
+//  Copyright © 2019 Yihua Zhou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
